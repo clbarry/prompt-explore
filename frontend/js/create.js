f19aad528203e0417a8c681e919992e956cff4ff
clbarry <c.barry@northeastern.edu>
2026-06-15 19:01:43 -0400

    adding Modal to Create Page

---
+++ frontend/js/create.js
@@ -0,0 +1,10 @@
+/* This file is for JavaScript code related to the create.html page.*/
+
+/* https://getbootstrap.com/docs/5.3/components/modal/ */
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
