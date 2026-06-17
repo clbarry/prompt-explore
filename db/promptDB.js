3f40cfd0d0176e5de42b0316d1d914e0c7fafd6b
Julia Weppler <jnweppler@gmail.com>
2026-06-17 18:59:30 -0400

    Adding delete function as 2 separate CRUD operations

--- db/promptDB.js
+++ db/promptDB.js
@@ -5,7 +5,7 @@ function PromptExplorerDB() {
   const URI = "mongodb://localhost:27017";
   const DB_NAME = "promptexplore";
   const COLLECTION = "prompts";
-
+  const RECENTLY_DELETED = "recently_deleted";
   const connect = () => {
     const client = new MongoClient(URI);
     const prompts = client.db(DB_NAME).collection(COLLECTION);
@@ -35,6 +35,39 @@ function PromptExplorerDB() {
         { $inc: { [`rating.${rating}`]: 1 } },
       );
       return result;
+    } catch (err) {
+      throw err;
+    } finally {
+      await client.close();
+    }
+  };
+
+  me.createRecentlyDeleted = async (record) => {
+    const { client } = connect();
+    const recentlyDeleted = client.db(DB_NAME).collection(RECENTLY_DELETED);
+    try {
+      return await recentlyDeleted.insertOne({
+        ...record,
+        deletedAt: new Date(),
+      });
+    } catch (err) {
+      throw err;
+    } finally {
+      await client.close();
+    }
+  };
+
+  me.deletePrompt = async (promptId) => {
+    const { client, prompts } = connect();
+    try {
+      const id = new ObjectId(promptId);
+      const prompt = await prompts.findOne({ _id: id });
+      if (!prompt) return { deleted: false, reason: "not found" };
+
+      await me.createRecentlyDeleted(prompt);
+
+      await prompts.deleteOne({ _id: id });
+      return { deleted: true };
     } catch (err) {
       throw err;
     } finally {
