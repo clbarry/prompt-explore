cfe468cff69007179bf458dbaef83df72efc6dc6
clbarry <c.barry@northeastern.edu>
2026-06-16 22:33:33 -0400

    Moderator buttons

---
+++ frontend/js/moderator.js
@@ -0,0 +1,4 @@
+const btnDeletePrompt = document.getElementById("btn-DeletePrompt");
+const btnSaveEdits = document.getElementById("btn-SaveEdits");
+const btnApprovePrompt = document.getElementById("btn-ApprovePrompt");
+const btnSkipPrompt = document.getElementById("btn-SkipPrompt");
