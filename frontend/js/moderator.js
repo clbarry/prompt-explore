66363b81a143096d72b099117773d91efcd08ea5
clbarry <c.barry@northeastern.edu>
2026-06-16 23:01:08 -0400

    Update moderator.js

--- frontend/js/moderator.js
+++ frontend/js/moderator.js
@@ -1,4 +1,23 @@
+const btnLoadDeletedPrompt = document.getElementById("btn-LoadDeletedPrompt");
 const btnDeletePrompt = document.getElementById("btn-DeletePrompt");
 const btnSaveEdits = document.getElementById("btn-SaveEdits");
 const btnApprovePrompt = document.getElementById("btn-ApprovePrompt");
 const btnSkipPrompt = document.getElementById("btn-SkipPrompt");
+
+
+
+btnLoadDeletedPrompt.addEventListener("click", async () => {  
+});
+
+btnDeletePrompt.addEventListener("click", async () => {  
+});
+
+btnSaveEdits.addEventListener("click", async () => {  
+});
+
+btnApprovePrompt.addEventListener("click", async () => {  
+});
+
+btnSkipPrompt.addEventListener("click", async () => {  
+});
+    
